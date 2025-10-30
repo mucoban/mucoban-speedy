1526ced589b321a384f2a7d0e7623a144640b354
mucoban <webdeveloper.mucahitcoban@gmail.com>
2025-10-30 22:37:22 +0100

    Feature: Homepage first section

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,5 +1,57 @@
 <template>
-    <div>
-        <h1>This is home page</h1>
+    <div class="homep-section">
+        <div class="container">
+            <div class="row">
+                <div class="co-12 text-center p-5">   
+                    <div class="small-hl">Lorem ipsum dolor</div>
+                    <div class="medium-hl">Travel essentials tips</div>
+                </div>
+                <div class="col-6  py-5">
+                    <div class="">
+                        <div class="small-hl ">Lorem ipsum dolor</div>
+                        <div class="medium-hl">Travel essentials tips</div>
+                        <p class="">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididu nt ut labore et dolore magna aliqua. Ut enim. ad minim veniam 
+                        </p>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <img src="https://wanderland.qodeinteractive.com/wp-content/uploads/2019/10/blog-post-img-01.jpg" />
+                </div>
+                <div class="col-6">
+                    <img src="https://wanderland.qodeinteractive.com/wp-content/uploads/2019/10/blog-post-img-01.jpg" />
+                </div>
+                <div class="col-6 py-5">
+                    <div class="">
+                        <div class="small-hl ">Lorem ipsum dolor</div>
+                        <div class="medium-hl">Travel essentials tips</div>
+                        <p class="">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididu nt ut labore et dolore magna aliqua. Ut enim. ad minim veniam 
+                        </p>
+                    </div>
+                </div>
+                
+            </div>
+            
+        </div>
     </div>
+
 </template>
+
+<style lang="scss">
+
+    .homep-section {
+        margin: 30px 0;
+    }
+
+    .small-hl {
+        color: #aaa;
+        font-size: 20px;
+    }
+
+    .medium-hl {
+        color: #000;
+        font-size: 35px;
+    }
+
+</style>
