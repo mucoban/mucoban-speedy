5838ce505c251d424a3c986ad36e96697cdb4f67
mucoban <webdeveloper.mucahitcoban@gmail.com>
2025-10-30 22:19:50 +0100

    Feature: Add main.scss

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['usebootstrap']
+  modules: ['usebootstrap'],
+  css: ['~/assets/css/main.scss'],
 })
