8a902991528ea9c01ab7368dc573399de09cfab2
mucoban <webdeveloper.mucahitcoban@gmail.com>
2025-11-06 22:53:45 +0100

    Feature: Add bootstrap-vue-next

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,5 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.scss'],
+  modules: ['@bootstrap-vue-next/nuxt'],
+  css: [
+    'bootstrap/dist/css/bootstrap.min.css',
+    '~/assets/css/main.scss'
+  ]
 })
