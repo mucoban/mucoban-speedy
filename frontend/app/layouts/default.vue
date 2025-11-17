9e5df60589367bc8489bb4e469ecaf19fa3dd30b
mucoban <webdeveloper.mucahitcoban@gmail.com>
2025-11-17 23:53:36 +0100

    Feature: Create footer

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -17,10 +17,59 @@
         </div>
 
         <div class="footer">
-            <div class="container-fluid">
-                <div class="holder">
-                    <p>footer</p>
-                </div>           
+            <div class="up-line">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-3">
+                            <a href="./" class="bottom-logo">
+                                <img src="https://wanderland.qodeinteractive.com/wp-content/uploads/2019/12/logo-footer.png" alt="">
+                            </a>
+                            <div class="bottom-list">
+                                <div class="hl">About the blog</div>
+                                <div class="text">Lorem ipsum dolor sit amet, conse ctetur adipisicing elit, sed do eiusmod mas.</div>
+                            </div>
+                        </div>
+                        <div class="col-9 mb-4">
+                            <p class="remark">
+                                Based in Utah, USA, Wanderland is a blog by Markus Thompson. His posts
+                                explore outdoor experiences through photos and diaries with tips & tricks.
+                            </p>
+                            <div class="bottom-lists">
+                                <div class="bottom-list" v-for="value in [0,1,2,3]">
+                                    <div class="hl">List A</div>
+                                    <div class="items">
+                                        <div class="item"><a href="./">Item 1 abcabcabcabc</a></div>
+                                        <div class="item"><a href="./">Item 2</a></div>
+                                        <div class="item"><a href="./">Item 3</a></div>
+                                        <div class="item"><a href="./">Item 4</a></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>          
+                </div>
+            </div>
+            <div class="bottom-line">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <div class="socials">
+                                <span class="text">
+                                    Socials
+                                </span>
+                                <span class="slinks">
+                                    <a href="./" class="slink">f</a>
+                                    <a href="./" class="slink">i</a>
+                                    <a href="./" class="slink">t</a>
+                                    <a href="./" class="slink">y</a>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="col text-end">
+                            All Rights Reserved
+                        </div>
+                    </div>          
+                </div>
             </div>
         </div>
     </div>
@@ -61,11 +110,72 @@
     a { text-decoration: unset; }
 
     .footer {
-        background-color: black;
-        padding: 20px;
-        text-align: center;
+        background-color: rgb(59, 59, 59);
+        color: white;
 
         p { color: white; }
+
+        .up-line {
+            padding: 50px;
+        }
+
+        .bottom-line {
+            background-color: black;
+            color: #ddd;
+            display: flex; 
+            justify-content: space-between;
+            padding: 20px;
+        }
+
+        .socials {
+            .text { margin-right: 5px; }
+
+            .slink {
+                color: #ddd; 
+                margin-right: 5px;
+            }
+        }
+
+        .remark {
+            font-style: italic;
+            color: #aaa; 
+            margin-bottom: 46px;
+        }
+
+        .bottom-logo {
+            display: inline-block;
+            margin-bottom: 20px;
+
+            img {
+                max-width: 144px;
+            }
+        }
+
+        .bottom-lists {
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .bottom-list {
+            .hl {
+                font-size: 20px;
+                font-weight: bold;
+                text-transform: uppercase;
+                margin-bottom: 20px;
+            }
+
+            .text { color: #aaa; }
+
+            .items {}
+
+            .item {
+                color: white;
+                a {
+                    color: #ccc;
+                    text-decoration: none;
+                }
+            }
+        }
     }
 
 </style>
